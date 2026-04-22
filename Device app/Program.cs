09028c671a56969f1a22b78fa56266d4c6587eda
Balkaran Bassi <balkaranbassi@outlook.com>
2026-04-22 14:47:09 +1200

    Version 1.2 Fixed error on Device catergory when invalid is added it errors valid Devices Desktop Laptop other

--- Device app/Program.cs
+++ Device app/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.Design;
 using System.Data;
+using System.Diagnostics;
 using System.Net.Security;
 using System.Reflection.Metadata;
 using System.Text.RegularExpressions;
@@ -11,6 +12,7 @@ class Program
 {
     //global variables
     static string mostExpensiveDevice = "";
+    static List<string> DeviceCategories = new List<string> { "Laptop", "Desktop", "Other"};
 
     static void Main(string[] args)
     {
@@ -33,17 +35,17 @@ class Program
             // call OneDeivce method
             OneDevice();
 
-            
+
             Nextdevice = CheckProceed();
 
             Console.Clear();
 
         }
-        
+
 
     }
 
-  
+
     static void OneDevice()
     {
         //local variables 
@@ -61,8 +63,8 @@ class Program
         //user input the device name
         Console.WriteLine("Please input device name:");
         deviceName = Console.ReadLine();
-       
-            //user inputs device quantity amount
+
+        //user inputs device quantity amount
         Console.WriteLine("Please input a quantity");
         quantityOfDevice = Convert.ToDecimal(Console.ReadLine());
 
@@ -71,15 +73,13 @@ class Program
         devicePrice = Convert.ToDecimal(Console.ReadLine());
 
         //device catergory
-        Console.WriteLine("Please input device catergory");
-        deviceCatergory = Console.ReadLine();
-
+        deviceCatergory = CheckDevicecatergory();
         //calculate insurance amount
         if (quantityOfDevice > 5)
         {
             // first five will be insured at full cost
             insuranceAmount = 5 * devicePrice;
-            insuranceAmount += (quantityOfDevice - 5) * 0.9m * devicePrice; 
+            insuranceAmount += (quantityOfDevice - 5) * 0.9m * devicePrice;
 
             //remaining devices will be insured at 10% less of cost
 
@@ -105,7 +105,7 @@ class Program
         deviceSummary += $"{deviceName}\nTotal cost for {quantityOfDevice}  {deviceName} devices is = to {devicePrice:C}\nMonth\t\t\tvalue Loss\n{deprecationSummary} {deviceCatergory}: {devicePrice:C}";
         Console.WriteLine(deviceSummary);
 
-        
+
 
 
 
@@ -130,6 +130,27 @@ class Program
             }
 
         }
+    }
+
+    static string CheckDevicecatergory()
+    {
+        while (true)
+        {
+            string Devicecatergory;
+            Console.WriteLine("Please input device catergory");
+            Devicecatergory = Console.ReadLine();
+            if (DeviceCategories.Contains(Devicecatergory) && !string.IsNullOrEmpty(Devicecatergory))
+            {
+                return Devicecatergory;
+            }
+            else
+            {
+                Console.WriteLine("Error please use valid device type");
+            }
+
+        }
+
+
 
     }
 }
